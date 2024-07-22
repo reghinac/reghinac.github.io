60e10803905cc0637b7180f5eed89ca9ae5fab8f
reghinac <cahyareghina@gmail.com>
2024-07-22 17:56:46 +0700

    perubahan

--- script.js
+++ script.js
@@ -98,6 +98,32 @@ pages.forEach((_, index) => {
      }, 500)
 
     }, (index + 1) * 200 + 2100)
+}) 
+
+const servicesContent = document.querySelector(".services-content");
+const popup = document.querySelector(".popup-box");
+const popupCloseBtn = popup.querySelector(".popup-close-btn");
+const popupCloseIcon = popup.querySelector(".popup-close-icon");
+
+servicesContent.addEventListener("click", function(event){
+    if(event.target.tagName.toLowerCase() == "button"){
+        const content = event.target.parentElement;
+        const h3 = content.querySelector("h3").innerHTML;
+        const readMoreCont = content.querySelector(".read-more-cont").innerHTML;
+        popup.querySelector("h3").innerHTML = h3;
+        popup.querySelector(".popup-body").innerHTML = readMoreCont;
+        popupBox();
+    }
 })
 
+popupCloseBtn.addEventListener("click", popupBox);
+popupCloseIcon.addEventListener("click", popupBox);
 
+popup.addEventListener("click", function(event) {
+    if(event.target == popup) {
+       popupBox();
+    }
+})
+function popupBox() {
+    popup.classList.toggle("open");
+}
